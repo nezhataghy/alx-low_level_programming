29da227f0fd523a3261c985611e145c493331ed6
nezhataghy <nezhataghy@gmail.com>
2023-06-15 13:55:10 -0700

    Hello_world

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: C code that use printf function to print a specific string
+ *
+ * Return: Always 0 for (success)
+*/
+
+int main(void)
+{
+	printf("Size of a char : %lu byte(s)", sizeof(char));
+	printf("Size of a int : %lu byte(s)", sizeof(int));
+	printf("Size of a long int : %lu byte(s)", sizeof(long int));
+	printf("Size of a long long int : %lu byte(s)", sizeof(long long int));
+	printf("Size of a float : %lu byte(s)", sizeof(float));
+	return (0);
+}
