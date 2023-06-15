7a49e49909374862531d9d2e391595e8b87593cb
nezhataghy <nezhataghy@gmail.com>
2023-06-15 13:22:54 -0700

    Hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,15 @@
-#include<stdio.h>
-int main(){
-    puts("\"Programming is like building a multilingual puzzle");
-    return(0);
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: C code that use put function to print a specific string
+ *
+ * Return: Always 0 for (success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
