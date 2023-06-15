b91e7d8d0c73082a079523fcedd3efbc4e31c6aa
nezhataghy <nezhataghy@gmail.com>
2023-06-15 13:35:24 -0700

    Hello_world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: C code that use printf function to print a specific string
+ *
+ * Return: Always 0 for (success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+}
