1d960973b3b53955f9ee8f2f3e8aff3f9230e8c4
nezhataghy <nezhataghy@gmail.com>
2023-06-16 07:38:06 -0700

    variables_if_else_while

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Display all alphabet
+ *
+ * Return: Always 0 for (success)
+*/
+
+int main(void)
+{
+int i = 97;
+
+while (i < 123)
+{
+	if (i == 101 || i == 113)
+		i++;
+	putchar(i);
+	i++;
+}
+putchar('\n');
+return (0);
+}
