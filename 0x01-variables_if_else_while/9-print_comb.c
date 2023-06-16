2a41baec8b6be5a503908e89e6d2b9edc13f05aa
nezhataghy <nezhataghy@gmail.com>
2023-06-16 08:30:34 -0700

    variables_if_else_while

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Display numbers 0-9 using putchar
+ *
+ * Return: Always 0 for (success)
+*/
+
+int main(void)
+{
+int i = 48;
+
+while (i < 58)
+{
+	putchar(i);
+	if (i != 57)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	i++;
+}
+putchar('\n');
+return (0);
+}
