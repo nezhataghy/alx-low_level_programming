0792a60dc831ad676c7f6fe0918b620279a4ed13
nezhataghy <nezhataghy@gmail.com>
2023-06-16 06:07:57 -0700

    “variables_if_else_while”

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Display alphabet
+ *
+ * Return: Always 0 for (success)
+*/
+
+int main(void)
+{
+for (int i = 97; i < 123; i++)
+	putchar(i);
+	putchar('\n');
+	return (0);
+}
