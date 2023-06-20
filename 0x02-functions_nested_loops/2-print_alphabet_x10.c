4fa723ba31608de033f8a6c8a8ca47932bbc1f34
nezhataghy <nezhataghy@gmail.com>
2023-06-20 09:21:50 -0700

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet in lowercase 10 times
+ *
+ * Return: On success 0.
+ */
+
+void print_alphabet_x10(void)
+{
+int n;
+int i = 97;
+
+for (n = 0; n < 10; n++)
+{
+	for (i = 97; i < 123; i++)
+		_putchar(i);
+	_putchar('\n');
+}
+}
