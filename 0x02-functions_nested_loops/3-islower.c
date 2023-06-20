5e57542523570de90e3a943b7cdd268e8c9617f8
nezhataghy <nezhataghy@gmail.com>
2023-06-20 13:36:11 -0700

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * _islower - checks for lowercase character
+ * @c: checks the input char
+ *
+ * Return: 1 if the char `c` is lowercase.
+ * 0 is returned (Success)
+*/
+
+int _islower(int c)
+{
+	if (c < 123 && c > 96)
+		return (1);
+	return (0);
+}
