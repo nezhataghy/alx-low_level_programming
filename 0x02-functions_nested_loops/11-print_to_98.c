e9a6d4dbf96300b024ac725f2a84f3df635a8433
nezhataghy <nezhataghy@gmail.com>
2023-06-20 17:55:33 -0700

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - diplay numbers in order from n to 98
+ * @num: the input
+*/
+
+void print_to_98(int num)
+{
+int m;
+
+if (num < 99)
+{
+	for (m = num; m < 98; m++)
+		printf("%d, ", m);
+	printf("98\n");
+}
+else
+{
+for (m = num; m > 98; m--)
+	printf("%d, ", m);
+}
+}
