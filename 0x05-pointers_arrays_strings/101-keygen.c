28a0971c41faeef5a3e5cfbdc5e9a9ab15b83d69
nezhataghy <nezhataghy@gmail.com>
2023-06-26 12:47:52 -0700

    pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int nb;
+	char c;
+
+	srand(time(NULL));
+	while (nb <= 2645)
+	{
+		c = rand() % 128;
+		nb += c;
+		putchar(c);
+	}
+	putchar(2771 - nb);
+	return (0);
+}
