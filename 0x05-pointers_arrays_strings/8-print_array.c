5a13f351575182857411b920501af8ce66165d9e
nezhataghy <nezhataghy@gmail.com>
2023-06-26 09:43:45 -0700

    pointers_arrays_strings

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,10 +12,10 @@ int i;
 
 for (i = 0; i < n; i++)
 {
-	if (i != (n - 1))
-		printf("%d, ", a[i]);
-	else
+	if (i == (n - 1))
 		printf("%d", a[i]);
+	else
+		printf("%d, ", a[i]);
 }
 printf("\n");
 }
