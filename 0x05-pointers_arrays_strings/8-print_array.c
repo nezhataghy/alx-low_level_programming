4239bd483fe81571935dec4af207c40f19e15be3
nezhataghy <nezhataghy@gmail.com>
2023-06-26 09:24:24 -0700

    pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - print array of integers
+ * @a: input
+ * @n: input
+*/
+
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < n - 1; i++)
+	printf("%d, ", a[i]);
+printf("%d\n", a[n - 1]);
+}
