5203d9f1e6565d093fc4e1912fa26994bf8868a0
nezhataghy <nezhataghy@gmail.com>
2023-06-29 15:33:33 -0700

    pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * *_strcat - appends the src string
+ * to the dest
+ * @dest: input
+ * @src: input
+ * Return: pointer
+*/
+
+char *_strcat(char *dest, char *src)
+{
+int l, l2;
+
+l = 0;
+
+while (dest[l])
+	l++;
+for (l2 = 0; src[l2]; l2++)
+	dest[l++] = src[l2]
+
+return (dest);
+}
