686907690f79d3bb956e731aeb260dd7ea2510b2
nezhataghy <nezhataghy@gmail.com>
2023-07-19 16:49:06 -0700

    function_pointers

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - prints the opcodes
+ * @argc: args count
+ * @argv: array of pointerrs
+ * Return: always O for success
+ */
+int main(int argc, char **argv)
+{
+int n_bytes, num;
+char *p = (char *)&main;
+
+if (argc != 2)
+{
+	printf("Error\n");
+	exit(1);
+}
+n_bytes = atoi(argv[1]);
+if (n_bytes < 0)
+{
+	printf("Error\n");
+	exit(2);
+}
+for (num = 0; num < n_bytes; num++)
+{
+	printf("%.2hhx", a[i]);
+	if (i < n - 1)
+		printf(" ");
+}
+printf("\n");
+return (0);
+}
