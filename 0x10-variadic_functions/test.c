40519233e02928b971b0dd188deeaac5e501bea2
nezhataghy <nezhataghy@gmail.com>
2023-07-27 13:40:08 -0700

    singly_linked_lists

---
+++ 0x10-variadic_functions/test.c
@@ -0,0 +1,51 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - Prints all of the arguments when specified
+ * @format: specifies the necessary operations
+ * Return: void
+ */
+
+void print_all(const char * const format, ...)
+{
+	int index = 0;
+	int flag;
+	char *chaine;
+	va_list list_ptr;
+
+	va_start(list_ptr, format);
+	while (format != NULL && format[index] != '\0')
+	{
+		if (format[index] == 'c')
+		{
+			printf("%c", va_arg(list_ptr, int));
+			flag = 0;
+		}
+		else if (format[index] == 'i')
+		{
+			printf("%i", va_arg(list_ptr, int));
+			flag = 0;
+		}
+		else if (format[index] == 'f')
+		{
+			printf("%f", va_arg(list_ptr, double));
+			flag = 0;
+		}
+		else if (format[index] == 's')
+		{
+			chaine = va_arg(list_ptr, char*);
+			if (chaine == NULL)
+				chaine = "(nil)";
+			printf("%s", chaine);
+			flag = 0;
+		}
+		else
+				flag = 1;
+		
+		if (format[index + 1] != '\0' && flag == 0)
+			printf(", ");
+		index++;
+	}
+	printf("\n");
+	va_end(list_ptr);
+}
