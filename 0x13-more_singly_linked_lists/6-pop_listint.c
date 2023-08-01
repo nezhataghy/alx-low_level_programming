fdf49afcfddeaa497f3c12161e6ada0d696daf49
nezhataghy <nezhataghy@gmail.com>
2023-08-01 17:12:25 +0000

    more_singly_linked_lists

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -2,17 +2,18 @@
 /**
  * pop_listint - deletes the head node
  * @head: double pointer head as input
+ * Return: head node’s data (n)
  */
 int pop_listint(listint_t **head)
 {
-    listint_t *ptr;
-    int node_data;
+	listint_t *ptr;
+	int node_data;
 
-    if (head == NULL || *head == NULL)
-        return (0);
-    ptr = *head;
-    *head = ptr->next;
-    node_data = ptr->n;
-    free(ptr);
-    return (node_data);
+	if (head == NULL || *head == NULL)
+		return (0);
+	ptr = *head;
+	*head = ptr->next;
+	node_data = ptr->n;
+	free(ptr);
+	return (node_data);
 }
