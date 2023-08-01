621619f3d4eab582a3c51264adfcee149fc45f99
nezhataghy <nezhataghy@gmail.com>
2023-08-01 13:16:06 +0000

    more_singly_linked_lists

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -5,13 +5,13 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *ptr = *head;
+	listint_t *ptr;
 
 	if (head == NULL)
 		return;
 	for (; *head;)
 	{
-		ptr = ptr->next;
+		ptr = (*head)->next;
 		free(*head);
 		*head = ptr;
 	}
