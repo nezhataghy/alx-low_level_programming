f325712952dac1bdbbdc2ea13a2d46cf181f19de
nezhataghy <nezhataghy@gmail.com>
2023-08-01 18:57:00 +0000

    more_singly_linked_lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -33,7 +33,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		traverse = (*head)->next;
 		idx--;
-		*head = traverse
+		*head = traverse;
 	}
 
 	if (!traverse)
