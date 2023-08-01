904b43afd16fb48d4e7866268909389e5644030e
nezhataghy <nezhataghy@gmail.com>
2023-08-01 18:55:55 +0000

    more_singly_linked_lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	new_node = malloc(sizeof(listint_t));
 	if (!new_node)
+	{
 		return (NULL);
+	}
 
 	new_node->n = n;
 
@@ -31,6 +33,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		traverse = (*head)->next;
 		idx--;
+		*head = traverse
 	}
 
 	if (!traverse)
