d56a7d2b08b345558766634946ca1afb66679ea4
nezhataghy <nezhataghy@gmail.com>
2023-08-08 01:39:41 +0000

    file_io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * read_textfile -  reads a text file and prints it to the POSIX standard output.
+ * read_textfile -  reads a text file and prints it to the
+ * POSIX standard output.
  * @filename: the file name
  * @letters: the number of letters
  *
@@ -10,8 +11,8 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t num_letters;
-	ssize_t num_written;
+	ssize_t num_l;
+	ssize_t num_wr;
 	char *file_content;
 
 	if (!filename)
@@ -22,10 +23,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
-	num_letters = read(fd, file_content, letters);
-	num_written = write(STDOUT_FILENO, file_content, num_letters);
+	num_l = read(fd, file_content, letters);
+	num_wr = write(STDOUT_FILENO, file_content, num_l);
 
-	if (fd < 0 || num_letters < 0 || num_written < 0 || num_letters != num_written)
+	if (fd < 0 || num_l < 0 || num_wr < 0 || num_l != num_wr)
 	{
 		free(file_content);
 		return (0);
@@ -34,5 +35,5 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	free(file_content);
 	close(fd);
 
-	return (num_written);
+	return (num_wr);
 }
