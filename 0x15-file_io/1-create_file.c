8f2ec2f7831dd4aba5da5bf2204f696b326ae6ea
nezhataghy <nezhataghy@gmail.com>
2023-08-08 02:19:54 +0000

    file_io

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * create_file -  creates a file.
+ * @filename: the file name
+ * @text_content: is the file content
+ *
+ * Return: 1 on success OTHERWISE return -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+    int fd;
+    int num_written;
+
+    if (!filename)
+        return (-1);
+
+    fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+    if (fd < 0)
+        return (-1);
+
+    num_written = write(fd, text_content, _strlen(text_content));
+    if (num_written < 0)
+        return (-1);
+
+    close(fd);
+
+    return (1);
+}
+
+/**
+ * _strlen - checks the length of a string
+ * @s: string to check
+ *
+ * Return: int length of the string
+ */
+
+int _strlen(char *s)
+{
+    int length = 0;
+
+    while (s[length])
+        length++;
+    return (length);
+}
