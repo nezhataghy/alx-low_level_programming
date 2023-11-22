692f5f2066ec251590a5ad9ba23f3ac80c57fda1
nezhataghy <nezhataghy@gmail.com>
2023-11-22 23:24:58 +0000

    Island Perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+def island_perimeter(grid):
+    '''
+        Calculates the perimeter of an island that is surrounded by eau
+        the Land is represented by 1 and the eau is by 0.
+    '''
+    perimeter = 0
+    eau = 0
+    terre = 1
+
+    for y, ligne in enumerate(grid):
+        for x, colonne in enumerate(ligne):
+            if colonne == terre:
+                if x == 0 or grid[y][x - 1] == eau:
+                    perimeter += 1
+                if (x + 1) == len(ligne) or grid[y][x + 1] == eau:
+                    perimeter += 1
+                if y == 0 or grid[y - 1][x] == eau:
+                    perimeter += 1
+                if (y + 1) == len(grid) or grid[y + 1][x] == eau:
+                    perimeter += 1
+
+    return perimeter
